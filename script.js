f3120ee96aae2d9f053cb8140ce1f2fc18edf582
IlliaLev <illialevuoi@gmail.com>
2025-09-29 19:32:01 +0300

    created setPlayerDisplay function

--- script.js
+++ script.js
@@ -175,6 +175,16 @@ const highlightCurrentSong = () => {
   }
 }
 
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+}
+
 renderSongs(sortSongs());
 
 playButton.addEventListener("click", () => {
